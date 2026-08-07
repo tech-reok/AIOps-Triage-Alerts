a4269abca31f214ff63ced47da0d41109bf18b36
Marco A. Cantu Gea <marco.cantu.g@gmail.com>
2026-08-07 16:05:01 -0600

    feat: Implement intentional startup failure based on environment variable

--- src/CsharpAppBuildDocker.Api/Program.cs
+++ src/CsharpAppBuildDocker.Api/Program.cs
@@ -23,4 +23,13 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
+// Fail startup intentionally when environment variable FAIL_ON_STARTUP=true
+var failOnStartup = Environment.GetEnvironmentVariable("FAIL_ON_STARTUP");
+if (!string.IsNullOrEmpty(failOnStartup) &&
+    failOnStartup.Equals("true", StringComparison.OrdinalIgnoreCase))
+{
+    // Throwing here will make the process exit with a non-zero code
+    throw new Exception("Startup failure triggered by FAIL_ON_STARTUP environment variable.");
+}
+
 app.Run();
